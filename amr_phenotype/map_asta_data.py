9eba25d5c56c9871244416943ea108e2ea104089
Nicole <nicolebo@umich.edu>
2024-09-25 14:44:48 -0500

    adding mapping ASTA data

---
+++ amr_phenotype/map_asta_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+### Now let's  map to the pathogen data from MicroBiggie ###
+contig_df = pd.read_csv("zou_plasmid_results.tsv", sep="\t")
+
+mb_df = pd.read_csv("/home/nbowers/bvbrc-dev/dev_container/NCBI_AMR_Codeathon/ncbi_pathogen_detect_amr.csv", low_memory=False)
+
+mb_df.rename(
+            columns={
+                "asm_acc": "Genome ID", 
+                "contig_acc": "Contig Name", 
+            },
+            inplace=True
+        )
+
+merged_df = pd.merge(contig_df, mb_df, how='left', on=['Genome ID', 'Contig Name'])
+merged_df.to_csv('mapped_zou_plasmid_results.tsv', sep="\t", index=False)
+
+#print(contig_df.columns)
+#print(merged_df.columns)
